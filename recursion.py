493fd8cae256bf46de00f6efc4f8a22ae05b9fbe
Saket-Rama <163721169+Saket-Rama@users.noreply.github.com>
2026-09-01 19:36:03 +0530

    completed basic concept of recursion

--- recursion.py
+++ recursion.py
@@ -1,5 +1,20 @@
+"""
 def f():
     print(1)
     f()
 print(f())
+"""
 # Stack overflow problem is based on this problem because we use recursion concept and we didn't end it so it stops the program after several outputs...
+
+# In order to stop this infinite recursion i need a base condition...
+"""
+cut=0
+def f():
+    global cut
+    if(cut==3):
+        return
+    print(cut)
+    cut+=1
+    return f()
+f()
+"""
