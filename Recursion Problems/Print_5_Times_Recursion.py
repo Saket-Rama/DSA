f24edf3273bc0de0221699b4b2e45bcb4aae64f5
Saket-Rama <163721169+Saket-Rama@users.noreply.github.com>
2026-09-01 20:02:27 +0530

    Problem - 1

---
+++ Recursion Problems/Print_5_Times_Recursion.py
@@ -0,0 +1,25 @@
+"""
+count=0
+def name(n):
+    global count
+    if count==5:
+        return
+    else:
+        print(n)
+        count+=1
+    return name(n)
+
+name("Saket")
+"""
+
+def name(count,n):
+    if count>n:
+        return
+    print("Saket")
+    return name(count+1,n)
+
+count=1
+n=int(input())
+name(count,n)
+
+# Recursion Tree f(1,4)->  f(2,4) -> f(3,4) -> f(4,4)...
