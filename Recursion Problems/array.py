0e6bd6961099066fc261d3124d707bce4e07dfe7
Saket-Rama <163721169+Saket-Rama@users.noreply.github.com>
2026-09-05 18:47:58 +0530

    two pointer

--- Recursion Problems/array.py
+++ Recursion Problems/array.py
@@ -8,5 +8,29 @@ def reverse_array(self,arr:list,n:int)->None:
 """
 
 """
-Using Recursion 
+Brute Force Approach
 """
+"""
+
+def reverse_array(arr:list,n:int)->None:
+    ar=[]
+    arr.reverse()
+    for i in arr:
+        ar.append(i)
+    print(ar)
+n=4
+reverse_array([1,2,3,4],n)
+"""
+"""
+Two Pointer Technique
+"""
+def reverse_array(arr):
+    p1=0
+    p2=len(arr)-1
+    while p1<p2:
+        arr[p1],arr[p2] = arr[p2],arr[p1]
+        p1+=1
+        p2-=1
+    arr2=list(arr)
+    print(arr2)
+reverse_array([1,2,3,4,5])
